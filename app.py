56153f0ab97f0c5602bc29ee7b282b44cb80b3c0
Manjot Singh <manjotsingh2004sm@gmail.com>
2026-07-30 16:10:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1778,7 +1778,8 @@ elif page == "RT Correction":
                 w*projection+(1-w)*df["Trend"],
                 df["Trend"]
             )
-    
+            
+            mask = actual > 0   
             pred=prediction[mask]
             act=actual[mask]
     
