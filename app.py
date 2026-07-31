5ace2acf128a684870f2367b46b2a37d44df3f4c
Manjot Singh <manjotsingh2004sm@gmail.com>
2026-07-31 12:18:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2166,8 +2166,7 @@ elif page == "Aeromal":
             x=np.arange(96),
             y=sym,
             name="Sym Profile",
-            line=dict(color="blue"),
-            line=dict(width=2)
+            line=dict(color="blue", width=2)
         )
     )
     
@@ -2176,8 +2175,7 @@ elif page == "Aeromal":
             x=np.arange(96),
             y=s,
             name="Profile",
-            line=dict(color="green"),
-            line=dict(width=2)
+            line=dict(color="green", width=2)
         )
     )
     
@@ -2186,8 +2184,7 @@ elif page == "Aeromal":
             x=np.arange(96),
             y=ap,
             name="95th Percentile",
-            line=dict(color="red"),
-            line=dict(width=2)
+            line=dict(color="red", width=2)
         )
     )
     
