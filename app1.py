a03776cf920baa802c02272c704d08ce713d5b6e
Manjot Singh <manjotsingh2004sm@gmail.com>
2026-08-03 18:02:41 +0530

    Update app1.py

--- app1.py
+++ app1.py
@@ -22,3 +22,68 @@ Forecast Correction Platform
 """, unsafe_allow_html=True)
 
 st.sidebar.divider()
+
+if "page" not in st.session_state:
+    st.session_state.page = "Loss Correction"
+
+if "aeromal_auth" not in st.session_state:
+    st.session_state.aeromal_auth = False
+
+pages = {
+    "⛅ Loss Correction": "Loss Correction",
+    "⏰ RT Correction": "RT Correction",
+    "🐱‍🏍 Aeromal": "Aeromal"
+}
+
+for label, page in pages.items():
+    if st.sidebar.button(
+        label,
+        use_container_width=True
+    ):
+        st.session_state.page = page
+
+st.sidebar.divider()
+
+st.sidebar.markdown("""
+<div style='text-align:center;color:gray;font-size:13px'>
+Developed & Maintained by<br>
+<b>Manjot Singh</b><br><br>
+
+Script Writer<br>
+<b>Tushar Sharma</b><br><br>
+
+Challenger<br>
+<b>Aarav Sharma</b><br><br>
+
+Tester<br>
+<b>Jatin Chaturvedi</b><br><br>
+
+Improviser<br>
+<b>Ujala Agrahari</b><br><br>
+
+Suggested by<br>
+<b>Garima Bajetha</b>
+</div>
+""", unsafe_allow_html=True)
+
+if st.session_state.aeromal_auth:
+
+    st.sidebar.markdown("---")
+
+    if st.sidebar.button(
+        "🚪 Logout",
+        use_container_width=True
+    ):
+        st.session_state.aeromal_auth = False
+        st.rerun()
+
+page = st.session_state.page
+
+if page == "Loss Correction":
+    show_loss_correction()
+
+elif page == "RT Correction":
+    show_rt_correction()
+
+elif page == "Aeromal":
+    show_aeromal()
