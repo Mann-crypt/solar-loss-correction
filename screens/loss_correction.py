76e689f5c8fac42add18f54f43e36d9125a34228
Manjot Singh <manjotsingh2004sm@gmail.com>
2026-08-04 11:08:33 +0530

    Update loss_correction.py

--- screens/loss_correction.py
+++ screens/loss_correction.py
@@ -3,4 +3,16 @@ import streamlit as st
 def show_loss_correction():
     st.title("⛅ Loss Correction")
 
+    uploaded_file = st.file_uploader("Yaha Feko!!",
+                                    type=["xlsx"],
+                                    key="excel_uploader")
+    
+    validate_uploaded_file(uploaded_file)
+    workbook = excel_reader(uploaded_file)
+    plant_type = st.segmented_control(
+        "Plant Type",
+        ["Fixed", "Tracking"]
+    )
+    if st.button("Run Correction"):
+
     st.info("Loss Correction page is under development.")
